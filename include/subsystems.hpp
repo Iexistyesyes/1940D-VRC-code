19246c07e388dec27014b64a584710bf57f44046
Iexistyesyes <lukelmills2012@gmail.com>
2026-08-29 20:51:55 +1000

    rwafawfa

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -10,7 +10,7 @@ extern Drive chassis;
 inline pros::Motor intake(5);
 inline pros::adi::DigitalIn limit_switch('A');
 // inline pros::Vision vision_sensor1(20);
-inline pros::MotorGroup chainbar({4, -7});
+inline pros::MotorGroup chainbar({-4, 7});
 inline ez::Piston Claw('A');
-inline pros::MotorGroup Cascade({1, 8});
+inline pros::MotorGroup Cascade({-1, 8});
 
